bf795fdba15c3f11e82052c99cd7d40d6066648e
vickywang042-commits <vicky.wang042@gmail.com>
2026-09-20 00:20:17 +0800

    Add files via upload

--- fcn-colleague/site/sw.js
+++ fcn-colleague/site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fcn-simple-android-v12";
+const CACHE_NAME = "fcn-simple-android-v13";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
